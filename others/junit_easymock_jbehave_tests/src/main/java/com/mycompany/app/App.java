5632df8a91197014721a8d0b005e2cffb72d1a09
ZIADI Mohamed ali <ma.ziadi@alphalink.fr>
2013-12-04 16:57:22 +0100

    ----vim---

---
+++ others/junit_easymock_jbehave_tests/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,15 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+  public Long calculer(Long a, Long b) {
+    return a+b;
+  }
+  public Character lireSymbole() {
+    return '-';
+  }
+}
